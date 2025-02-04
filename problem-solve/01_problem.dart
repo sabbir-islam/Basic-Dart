ee8ab225a5c988a3c15657e1278a8e15f0ded7db
SABBIR ISLAM <99163880+sabbir-islam@users.noreply.github.com>
2025-02-04 16:31:49 +0000

    Problem

---
+++ problem-solve/01_problem.dart
@@ -0,0 +1,13 @@
+void main(){
+    var num = 10;
+
+    if(num == 0){
+        print("number $num is zero");
+    }
+    else if(num < 0){
+        print("number $num is negative");
+    }
+    else{
+        print("number $num is positive");
+    }
+}
